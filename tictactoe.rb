46c1947cf66f94cedf442db47309dd5077c5e0b8
Kyle Doherty <kyledoherty22@gmail.com>
2013-04-03 18:46:06 -0400

    first tictactoe commit

---
+++ tictactoe.rb
@@ -0,0 +1,29 @@
+##### Instructions #####
+
+# 1) You can use whatever language you'd like (we suggest Ruby or Javascript).
+# 2) Use whatever coding environment you'd like (jsfiddle.net is a good place to start for a browser-based Javascript editor). You can even use TextEdit and just write code. Your choice.
+# 3) Use as many online resources as you like, but the challenge is meant to be completed alone, so please don't take any outside help.
+# 4) On the front-end, there is no need to make it pretty. We are looking to see how you approach solving it and breaking it down.
+
+
+
+##### Steps ######
+
+#1 - create a board that can hold Xs and Os - hash or array
+#2 - puts the board empty board and ask player 1 to make first move 
+#3 - take players input and add it to the appropriate place on the board (hash)
+#4 - puts out the updated board 
+#5 - ask player 2 to make second move 
+#6 - repeat step 3
+#7 - repeat step 4
+#8 - ask player 1 to make third move
+#9 - repeat step 3 (after get this all down add a step here to check for winner)
+#10 - repeat step 4
+#11 - ask player 2 to make fourth move
+#12 - repeat step 3
+#13 - repeat step 4
+#14 - ask player 1 to make fifth move 
+#15 - repeat step 3
+#16 - repeat step 4
+#17 - ask player 2 to make sixth move 
+#18 - continue with steps until get to 9th move
