21ea7c6ec981c97e4cba5e193147a109d8ed8703
Kyle Doherty <kyledoherty22@gmail.com>
2013-04-06 16:59:38 -0400

    don't allow players to overwrite moves that have already been made

--- tictactoe.rb
+++ tictactoe.rb
@@ -30,24 +30,6 @@
 	end
 
 		
-	
-
-
-
-#2 - puts the board empty board and ask player 1 to make first move 
-
-	
-
-	# def convert input #method to covert players input in appropriate array index
-	# 		case input
-	# 		when 1
-	# 			input = 0
-	# 		when 3
-	# 			input = 4
-	# 		end
-
-	# 	end
-		
 
 ###### method to add move to board and print board ######
 	def addmove(move, player)
@@ -87,21 +69,25 @@
 		end   
 
 
-		@board[row][col]= player
+		#add if statement to check if that array index already has a value.  if it does don't replace it and ask the player to make a different move
+
+		if @board[row][col] #already has a value 
+			puts "Sorry you can't make that move.  Please make a different move:"
+				new_move = gets.chomp.to_i
+				addmove(new_move,player)
+		else
+			@board[row][col]= player
+
+		end
 
 		show_board
 
-		# puts @board[0].join.center(@line_width) #remembered the part in Chris Pine's book about visual formatting 
-		# puts @board[1].join.center(@line_width)
-		# puts @board[2].join.center(@line_width)
-		# puts @board[3].join.center(@line_width)
-		# puts @board[4].join.center(@line_width) 
 
 	end
 
 
 		
-		show_board
+		show_board #shows the board at the start of the game
 
 	9.times do |x|
 
@@ -116,21 +102,7 @@
 		end
 
 
-		# puts "Make your move by selecting the row (0-2) and column (0-2)"
-		# puts "Select the row:"
-
-		# puts 'Make your move by selecting a space on the board 1-9.  They are numbered left to right.'
-		# puts 'Make your move:'
-
 		move = gets.chomp.to_i
-
-		# row = gets.chomp.to_i
-
-		# puts "Please the column:"
-
-		# col = gets.chomp.to_i
-
-		# addmove(row,col)
 
 		addmove(move, player)
 
@@ -150,19 +122,3 @@
 	
 
 		
-#3 - take players input and add it to the appropriate place on the board (hash)
-#4 - puts out the updated board 
-#5 - ask player 2 to make second move 
-#6 - repeat step 3
-#7 - repeat step 4
-#8 - ask player 1 to make third move
-#9 - repeat step 3 (after get this all down add a step here to check for winner)
-#10 - repeat step 4
-#11 - ask player 2 to make fourth move
-#12 - repeat step 3
-#13 - repeat step 4
-#14 - ask player 1 to make fifth move 
-#15 - repeat step 3
-#16 - repeat step 4
-#17 - ask player 2 to make sixth move 
-#18 - continue with steps until get to 9th move
